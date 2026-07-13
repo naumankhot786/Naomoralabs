a228a59c36f2b5f1a8b81f3a6fe61b6962f0e0a9
naumankhot786 <naumankhot786@gmail.com>
2026-07-13 15:33:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -157,14 +157,4 @@ chatBtn.onclick = () => {
     chatBox.style.display = "block";
   }
 };
-window.addEventListener("load", function () {
 
-    setTimeout(function () {
-
-        const loader = document.getElementById("loader");
-        loader.classList.add("loader-hide");
-        loader.style.display = "none";
-
-    }, 1500);
-
-});
